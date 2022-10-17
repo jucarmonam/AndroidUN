4be5e257376055453530d9f54ba2c16d9666bed4
Juan <jucarmonam@unal.edu.co>
2022-10-17 11:30:44 -0500

    inicio reto 10

---
+++ reto 10/app/src/main/java/com/example/reto10/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.reto10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.reto10.companyList.APIService
+import com.example.reto10.ui.theme.Reto10Theme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Reto10Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //Greeting("Android")
+                }
+            }
+        }
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://www.datos.gov.co/resource/8hn7-rpp8.json/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(
+                APIService::class.java).getDogsByBreeds("$query/images")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Reto10Theme {
+        Greeting("Android")
+    }
+}
---
+++ reto 10/app/src/main/java/com/example/reto10/companyList/CompanyResponse.kt
@@ -0,0 +1,3 @@
+package com.example.reto10.companyList
+
+data class CompanyResponse(var no : String, var nit: String, var razon_social: String, var ciiu: String, )
